e1f9f9bd5ecfc425ece8231f33ae0bc5ec3ebfc3
gaols <igaols@foxmail.com>
2019-03-02 14:16:15 +0800

    fix xml serialize

--- src/test/java/com/github/gaols/unipay/api/WxNotifyResponseTest.java
+++ src/test/java/com/github/gaols/unipay/api/WxNotifyResponseTest.java
@@ -1,11 +1,18 @@
 package com.github.gaols.unipay.api;
 
+import com.github.gaols.unipay.utils.XmlUtils;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Map;
 
 public class WxNotifyResponseTest {
 
     @Test
     public void toXml() {
-        System.out.println(WxNotifyResponse.fail("fail"));
+        String xmlData = WxNotifyResponse.fail("fail");
+        Map<String, String> map = XmlUtils.parseXml(xmlData);
+        Assert.assertEquals("FAIL", map.get("return_code"));
+        Assert.assertEquals("fail", map.get("return_msg"));
     }
 }
